d2bf7ffa266d4d952411f2dd3dc83b4b9c379bf8
Yareslav <80545801+Yareslav@users.noreply.github.com>
2022-04-12 20:20:00 +0300

    he

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+namespace comnsole2
+{
+    class Program
+    {
+        static int iterations = 10;
+        static int currentIteration = 0;
+        static void Main(string[] args)
+        {
+            Recurs();
+        }
+        static void Recurs()
+        {
+            currentIteration++;
+            Console.WriteLine(currentIteration);
+            var thread=new Thread(delegate ()
+            {
+                Thread.Sleep(1000);
+                if (currentIteration < iterations) Recurs();
+            });
+            thread.Start();
+        }
+    }
+}
